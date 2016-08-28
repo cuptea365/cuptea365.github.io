a9e19fda4ebd6fc09770c3535e68b06182849c9a
litteGhost <cuptea365@outlook.com>
2016-08-28 15:14:45 +0800

    add test.js

---
+++ javascripts/test.js
@@ -0,0 +1,12 @@
+function run() {
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id", placeholder);
+    placeholder.setAttribute("src", "images//18.png");
+    placeholder.setAttribute("alt", "18png");
+    var ins = document.getElementById("ins");
+    ins.parentNode.insertBefore(placeholder, ins);
+}
+
+window.onload = function() {
+    run();
+}
